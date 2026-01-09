47b2103fd23079334a20f66d9be51ae04a3c491f
“carlosrobertodevops” <carlos.roberto.devops@gmail.com>
2026-01-09 11:30:34 -0300

    Ajustes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,7 @@ import { NextResponse } from "next/server";
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (
-    isAdminRoute(req) &&
-    (await auth()).sessionClaims?.metadata?.role !== "admin"
-  ) {
+  if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== "admin") {
     const url = new URL("/", req.url);
     return NextResponse.redirect(url);
   }
